b4a0994ab555cf58c851949e815c2f3de433a4a6
Ujwala <ujwalap801@gmail.com>
2025-08-30 13:26:57 +0530

    Added backend_URL

--- student-Frontend/src/context/StudentProvider.jsx
+++ student-Frontend/src/context/StudentProvider.jsx
@@ -1,7 +1,7 @@
 import {useState, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/students";
+const API_URL = "https://student-management-backend-0u0b.onrender.com/students";
 
 import { StudentContext } from "./StudentContext";
 
